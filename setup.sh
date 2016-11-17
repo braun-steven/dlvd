2b53f0888e733b37601cf29b590fe5a33c3b6b22
Steven Lang <slang03@students.uni-mainz.de>
2016-11-17 17:00:46 +0100

    Update setup and README
    	modified:   README.md
    	modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@ cd data/datasets/
 wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
 echo -e "Extracting datasets"
 tar -xvf cifar-10-python.tar.gz
+cd ../../
+echo -e "Building docker image"
+docker build -t tensorflow:tutorial docker/
