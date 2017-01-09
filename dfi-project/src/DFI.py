bd90caa81dae72cd7b9bbe3d256233f42a2538ae
Steven Lang <slang03@students.uni-mainz.de>
2017-01-09 23:41:47 +0100

    Documentation

--- dfi-project/src/DFI.py
+++ dfi-project/src/DFI.py
@@ -1,6 +1,7 @@
 from time import time
 
 import tensorflow as tf
+from __builtin__ import xrange
 from sklearn.neighbors import KNeighborsClassifier
 from tensorflow.contrib.opt import ScipyOptimizerInterface
 
@@ -9,14 +10,27 @@ from vgg19 import Vgg19
 
 
 class DFI:
-    """
+    """Deep Feature Interpolation procedure
 
+    Implementation of DFI as described here:
+    https://arxiv.org/pdf/1611.05507v1.pdf
     """
 
     def __init__(self, k=10, alpha=0.4, lamb=0.001, beta=2,
                  model_path="./model/vgg19.npy", num_layers=3,
                  gpu=True, data_dir='./data'):
-        # Set variables
+        """
+        Initialize the DFI procedure
+        :param k: Number of nearest neighbours
+        :param alpha: Scalar factor for w
+        :param lamb: Scalar factor for total variation in loss
+        :param beta: Scalar in exponent of total variation
+        :param model_path: Path to the vgg19 pretrained model
+        :param num_layers: Number of layers of which the deep
+        features shall be extracted
+        :param gpu: Compute or gpu
+        :param data_dir: Directory for images
+        """
         self._k = k
         self._alpha = alpha
         self._beta = beta
@@ -43,6 +57,12 @@ class DFI:
         print('Initialization finished')
 
     def run(self, feat='No Beard', person_index=0):
+        """
+
+        :param feat: Attribute
+        :param person_index: Index of start image
+        :return:
+        """
         print('Starting DFI')
         # Config for gpu
         config = tf.ConfigProto()
@@ -104,9 +124,15 @@ class DFI:
             np.save('z', z)
 
     def _minimize_z_tensor(self, phi_z, tf_z):
+        """
+        Objective function implemented with tensors
+        :param phi_z: phi(x) + alpha*w
+        :param tf_z: Variable
+        :return: loss
+        """
         # Init z with the initial guess
         tf_phi_z = tf.constant(phi_z, dtype=tf.float32)
-        phi_tf = self._phi_tensor(tf_z)
+        phi_tf = self._phi_tensor()
         subtract = tf.subtract(phi_tf, tf_phi_z)
         square = tf.square(subtract)
         reduce_sum = tf.reduce_sum(square)
@@ -117,7 +143,8 @@ class DFI:
         return loss
 
     def _total_variation_regularization(self, x, beta=1):
-        """ Idea from:
+        """
+        Idea from:
         https://github.com/antlerros/tensorflow-fast-neuralstyle/blob/master/net.py
         """
         wh = tf.constant([[[[1], [1], [1]]], [[[-1], [-1], [-1]]]], tf.float32)
@@ -131,16 +158,21 @@ class DFI:
         return tv
 
     def _conv2d(self, x, W, strides=[1, 1, 1, 1], p='SAME', name=None):
+        """2d Convolution"""
         new_shape = tf.TensorShape([tf.Dimension(1)] + x.get_shape().dims)
         x = tf.reshape(x, new_shape)
         return tf.nn.conv2d(x, W, strides=strides, padding=p, name=name)
 
-    def _phi_tensor(self, img):
+    def _phi_tensor(self):
+        """
+        Implementation of the deep feature function in tensorflow
+        :return: tensor of z in the deep feature space
+        """
         # Start with first tensor
         res = tf.reshape(self._conv_layer_tensors[0], [-1])
 
         # Concatenate the rest
-        for i in range(1, self._num_layers):
+        for i in xrange(1, self._num_layers):
             tmp = tf.reshape(self._conv_layer_tensors[i], [-1])
             res = tf.concat(0, [res, tmp])
         return res
@@ -184,8 +216,15 @@ class DFI:
             return [np.linalg.norm(x) for x in res]  # List of images
 
     def _minimize_z(self, z, phi_z, lamb, beta):
+        """
+        Calculates the loss as described in the paper
+        :param z: Objective variable
+        :param phi_z: phiz
+        :param lamb: lambda
+        :param beta: beta
+        :return: loss
+        """
         # Reshape into image form
-
         z = z.reshape(224, 224, 3)
 
         loss = 0.5 * np.linalg.norm(phi_z - self._phi(z)) ** 2
@@ -197,8 +236,8 @@ class DFI:
         return res
 
     def _R(self, z, beta):
-        """Total Variation regularizer
-
+        """
+        Total Variation regularizer
         :param z: objective
         :param beta: beta
         :return: R
@@ -217,6 +256,13 @@ class DFI:
         return result
 
     def _get_sets(self, atts, feat, person_index):
+        """
+        Generates the subsets of the knn given a person index and a feature
+        :param atts: Attribute dataframe
+        :param feat: Feature
+        :param person_index: Person index
+        :return: Positive and negative subset of the k nearest neighbours of the given person
+        """
         person = atts.loc[person_index]
         del person['person']
         del person['path']
@@ -233,6 +279,12 @@ class DFI:
         return pos_paths.as_matrix(), neg_paths.as_matrix()
 
     def _get_k_neighbors(self, subset, person):
+        """
+        Gets the KNN, given the subset and the person
+        :param subset: Subset in which the knn should look at
+        :param person: Starting point
+        :return: Image paths of the k nearest neighbours of the person in the subset
+        """
         del subset['person']
         paths = subset['path']
         del subset['path']
@@ -248,5 +300,9 @@ class DFI:
         return neighbor_paths
 
     def attributes(self):
+        """
+        Generate a list of possible attributes to choose from
+        :return: List of attributes
+        """
         print('TODO: Implement')
         pass
